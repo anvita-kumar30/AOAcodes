7a0fdbfc248e833a05f9ae016193cb0a70a89e0b
Anvita Kumar✨ <kumaranvita30@gmail.com>
2023-04-25 22:10:03 +0530

    init

--- SelectionSort.c
+++ SelectionSort.c
@@ -15,32 +15,35 @@ void displayArray(int n, int array[n])
         printf("%d\t", array[i]);
     }
 }
-void insertionSort(int n, int array[n])
+void selectionSort(int n, int array[n])
 {
-    for (int j = 1; j < n; j++)
+    for (int i = 0; i < n; i++)
     {
-        int key = array[j];
-        int i = j - 1;
-        while ((i >= 0) && (array[i] > key))
+        int j = i;
+        for (int k = i + 1; k < n; k++)
         {
-            array[i + 1] = array[i];
-            i = i - 1;
+            if (array[k] < array[j])
+            {
+                j = k;
+            }
         }
-        array[i + 1] = key;
-        printf("\n\nPass Number %d : ", j);
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        printf("\n\nPass Number %d : ", i + 1);
         displayArray(n, array);
     }
 }
 void main()
 {
     int n;
-    int array[n];
     printf("Enter the array size: ");
     scanf("%d", &n);
+    int array[n];
     readArray(n, array);
     printf("\nBefore swapping the array elements are as follows:\n");
     displayArray(n, array);
-    insertionSort(n, array);
+    selectionSort(n, array);
     printf("\n\nAfter swapping the array elements are as follows:\n");
     displayArray(n, array);
 }
