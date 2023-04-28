f8aec9cdf00f2015ce885cd3d8897e1592e60f0d
Anvita Kumar✨ <kumaranvita30@gmail.com>
2023-04-28 20:58:13 +0530

    init

--- FractionalKnapsack.c
+++ FractionalKnapsack.c
@@ -3,7 +3,7 @@ int m,n;
 float x[10];
 struct knaps
 {
-	int w,p,obj_no;
+	int w, p, obj_no;
 	float pw;
 };
 
@@ -15,7 +15,7 @@ void sortks()
     struct knaps temp;
     for (i = 0; i < n - 1; i++)
     {
-        for (j = 0; j < (n - 1-i); j++)
+        for (j = 0; j < (n - 1 -i); j++)
         {
             if (arr[j].pw < arr[j + 1].pw)
             {
@@ -28,22 +28,22 @@ void sortks()
 } 
 int greedyknapsack()
 {
-	int i,rem_cap,profit=0;
-	for(i=0;i<n;i++)
+	int i, rem_cap, profit=0;
+	for(i=0; i<n; i++)
 		x[i]=0;
 	rem_cap=m;
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 	{
-		if(arr[i].w>rem_cap)
+		if(arr[i].w > rem_cap)
 			break;
-		x[arr[i].obj_no-1]=1;
-		profit=profit+arr[i].p;
-		rem_cap=rem_cap-arr[i].w;	
+		x[arr[i].obj_no-1] = 1;
+		profit = profit + arr[i].p;
+		rem_cap = rem_cap - arr[i].w;	
 	}	
 	if(i<=n)
 	{
 		x[arr[i].obj_no-1]=(float)(rem_cap)/(float)(arr[i].w);
-		profit=profit+x[i]*(arr[i].p);
+		profit = profit + x[arr[i].obj_no-1]*(arr[i].p);
 	}	
 	return profit;	
 }
@@ -55,41 +55,41 @@ int main()
 	printf("Enter the number of objects: ");
 	scanf("%d",&n);
 	printf("\nEnter Weights:\n");
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 	{
-		printf("Enter weight of object %d: ",i+1);
-		scanf("%d",&arr[i].w);
+		printf("Enter weight of object %d: ", i+1);
+		scanf("%d", &arr[i].w);
 	}
 	printf("\nEnter Profits:\n");
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 	{
-		printf("Enter profit of object %d: ",i+1);
-		scanf("%d",&arr[i].p);
+		printf("Enter profit of object %d: ", i+1);
+		scanf("%d", &arr[i].p);
 	}
-	for(i=0;i<n;i++)
-		arr[i].obj_no=i+1;
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
+		arr[i].obj_no = i+1;
+	for(i=0; i<n; i++)
 	{
 		arr[i].pw = (float)(arr[i].p)/(float)(arr[i].w);
 	}
 	printf("\nCalculating Ratio");
 	printf("\nObject\tWeight\t\tProfit\t\tRatio\n");
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 	{
-		printf("%d\t%d\t\t%d\t\t%f\n",i+1,arr[i].w,arr[i].p,arr[i].pw);
+		printf("%d\t%d\t\t%d\t\t%f\n",arr[i].obj_no,arr[i].w,arr[i].p,arr[i].pw);
 	}
 	sortks();
 	printf("\nAfter sorting");
 	printf("\nObject\tWeight\t\tProfit\t\tRatio\n");
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 	{
-		printf("%d\t%d\t\t%d\t\t%f\n",i+1,arr[i].w,arr[i].p,arr[i].pw);
+		printf("%d\t%d\t\t%d\t\t%f\n",arr[i].obj_no,arr[i].w,arr[i].p,arr[i].pw);
 	}
 	value=greedyknapsack();
     printf("\nSolution Vector=[");
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 		printf("%0.2f\t",x[i]);
     printf("\b\b\b\b\b]");
-	printf("\nTotal Profit= %d",value);
+	printf("\nTotal Profit= %d", value);
 	return 0;
 }
